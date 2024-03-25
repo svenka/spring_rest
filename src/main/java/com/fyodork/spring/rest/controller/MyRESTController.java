04f3d1a7f5dca21647595fea736cd447c6dcfbd2
svenka <svenkagrip@gmail.com>
2024-03-25 22:38:29 +0800

    Exception handling

--- src/main/java/com/fyodork/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/fyodork/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,13 @@
 package com.fyodork.spring.rest.controller;
 
 import com.fyodork.spring.rest.entity.Employee;
+import com.fyodork.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.fyodork.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.fyodork.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,8 +29,31 @@ public class MyRESTController {
     public Employee getEmployee(@PathVariable int id)
     {
         Employee employee=employeeService.getEmployee(id);
+
+        if(employee==null)
+        {
+            throw  new NoSuchEmployeeException("Не найден Сотрудник с id="+id+" в БД");
+        }
         return employee;
     }
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+
+        EmployeeIncorrectData data=new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+
+        EmployeeIncorrectData data=new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+
+    }
+
 
 
 }
